8a06e91a0b8f1ba9008fd356b4f2f484a4f6da5d
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-03-18 23:14:59 -0400

    commit

---
+++ lab-3/js/lab3.js
@@ -0,0 +1,14 @@
+// const tags = document.querySelectorAll("tabpanel");
+
+// tabpanel.forEach((tab) => {
+//     tab.addEventListener("toggle", (event) => {
+//         if (event.target.open {
+//             details.forEach ((tab) => {
+//                 if(tab !== event.target) {
+//                     tab.removeAttribute("open");
+//                 }
+//             })
+//         })
+//     })
+// })
+    
